2decf191f5d0fa712d9be7051a935bb6c847178c
priyankagarad <garadpriyanka1995@gmail.com>
2020-05-17 19:07:35 +0530

    ADDED:Test Case for null Check

--- src/test/java/com/bl/measurment/QuantityMeasurmentTest.java
+++ src/test/java/com/bl/measurment/QuantityMeasurmentTest.java
@@ -3,7 +3,6 @@ import com.bl.quantityMeasurment.QuantityMeasurment;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
 public class QuantityMeasurmentTest {
     QuantityMeasurment quantityMeasurment;
 
@@ -18,5 +17,12 @@ public class QuantityMeasurmentTest {
         double value1=quantityMeasurment.getFeetValue(0.0);
         double value2=quantityMeasurment.getFeetValue(0.0);
         Assert.assertEquals(value1,value2,0.0);
+    }
+
+    @Test
+    public void givenFeetValue_WhenNull_ThenShouldReturnFalse()
+    {
+        boolean checkNullValue=quantityMeasurment.equals(null);
+        Assert.assertEquals(false,checkNullValue);
     }
 }
