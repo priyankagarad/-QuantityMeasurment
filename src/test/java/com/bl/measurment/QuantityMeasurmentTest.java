3fe018985198550374e2e5b419f3df3687f002f4
priyankagarad <garadpriyanka1995@gmail.com>
2020-05-17 18:46:10 +0530

    ADDED:Test Case to Check Zero Feet Equality

--- src/test/java/com/bl/measurment/QuantityMeasurmentTest.java
+++ src/test/java/com/bl/measurment/QuantityMeasurmentTest.java
@@ -1,3 +1,22 @@
 package com.bl.measurment;
+import com.bl.quantityMeasurment.QuantityMeasurment;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class QuantityMeasurmentTest {
+    QuantityMeasurment quantityMeasurment;
+
+    @Before
+    public void setUp(){
+        quantityMeasurment=new QuantityMeasurment();
+    }
+
+    @Test
+    public void givenZeroFeetAndZeroFeetValue_WhenEqual_ThenShouldReturnTrue()
+    {
+        double value1=quantityMeasurment.getFeetValue(0.0);
+        double value2=quantityMeasurment.getFeetValue(0.0);
+        Assert.assertEquals(value1,value2,0.0);
+    }
 }
